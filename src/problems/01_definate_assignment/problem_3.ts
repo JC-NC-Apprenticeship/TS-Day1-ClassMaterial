8c9493286e8aa865ad454808bb4fab8e3215ab2f
Justin Heath <just.coder@outlook.com>
2021-08-09 09:54:30 +0100

    justin

--- src/problems/01_definate_assignment/problem_3.ts
+++ src/problems/01_definate_assignment/problem_3.ts
@@ -1,6 +1,15 @@
+/* // Task
 let price: unknown = 100;
 
 const isExpensive = (val: unknown) => {
+  return val > 50;
+};
+isExpensive(price);
+*/
+
+let price: number = 100;
+
+const isExpensive = (val: number) => {
   return val > 50;
 };
 isExpensive(price);
--- src/problems/01_definate_assignment/problem_4.ts
+++ src/problems/01_definate_assignment/problem_4.ts
@@ -1,6 +1,18 @@
+/* // Task
 let password: unknown = "P4ssw*rD";
 
 const isStrong = (password: unknown) => {
+  const hasNum = /[1-0]/.test(password);
+  const hasCapital = /[A-Z]/.test(password);
+  const hasSpecialChar = /[!@£$%^&*()?.,]/.test(password);
+  return hasNum && hasCapital && hasSpecialChar;
+};
+isStrong(password);
+*/
+
+let password: string = "P4ssw*rD";
+
+const isStrong = (password: string) => {
   const hasNum = /[1-0]/.test(password);
   const hasCapital = /[A-Z]/.test(password);
   const hasSpecialChar = /[!@£$%^&*()?.,]/.test(password);
